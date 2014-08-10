ddbf0db3d4b84fea43cbe726225a350b4544cc8b
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-08-10 11:31:28 -0400

    Can randomize luminosity, also display colors

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,14 +14,35 @@ angular.module('paletteApp')
       return tinycolor(colors[Math.floor(Math.random() * colors.length)]);
     };
 
+    $scope.luminosities = ['bright', 'light', 'dark'];
     $scope.options = {
-      source_color: get_random_color().toHex(),
-      copy_format: 'hex'
+      source_color: get_random_color().toHex(), luminosity: 'bright',
+      copy_format: 'hex', generate: 'schemes', source_hue: 'red'
     };
     $scope.toggle = {copy_message: false};
     $scope.palettes = [];
+    $scope.colors = [];
 
-    $scope.make_palettes = function() {
+    $scope.generate_palettes = function() {
+      if ($scope.options.generate === 'schemes') {
+        $scope.make_schemes();
+      } else {
+        $scope.make_colors();
+      }
+    };
+
+    $scope.make_colors = function() {
+      $scope.colors.length = 0;
+      var raw_colors = randomColor({count: 100, hue: $scope.options.source_hue,
+                                    luminosity: $scope.options.luminosity,
+                                    format: 'hex'});
+      for (var i=0; i<raw_colors.length; i++) {
+        $scope.colors.push(tinycolor(raw_colors[i]));
+      }
+      console.log($scope.colors);
+    };
+
+    $scope.make_schemes = function() {
       $scope.palettes.length = 0;
       var base_color = tinycolor($scope.options.source_color).toHsv();
       var scheme_types = ['analogous', 'monochromatic', 'complementary',
@@ -53,8 +74,25 @@ angular.module('paletteApp')
       }
     };
 
+    $scope.$watch('options.copy_format', function() {
+      var color = tinycolor($scope.options.source_color);
+      $scope.options.source_color = $scope.get_color_text(color);
+    });
+
     $scope.$watch('options.source_color', function() {
-      $scope.make_palettes();
+      $scope.make_schemes();
+    });
+
+    $scope.$watch('options.generate', function() {
+      $scope.generate_palettes();
+    });
+
+    $scope.$watch('options.source_hue', function() {
+      $scope.make_colors();
+    });
+
+    $scope.$watch('options.luminosity', function() {
+      $scope.make_colors();
     });
 
     $scope.get_color_list_height = function() {
@@ -67,9 +105,13 @@ angular.module('paletteApp')
       });
     });
 
-    $scope.randomize_source_color = function() {
-      var color = get_random_color();
-      $scope.options.source_color = $scope.get_color_text(color);
+    $scope.randomize = function() {
+      if ($scope.options.generate === 'schemes') {
+        var color = get_random_color();
+        $scope.options.source_color = $scope.get_color_text(color);
+      } else {
+        $scope.options.luminosity = $scope.luminosities[Math.floor(Math.random() * $scope.luminosities.length)];
+      }
     };
 
     $scope.get_color_text = function(color) {
